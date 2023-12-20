70f797bf3f2c9c4f905fb7e2d6780eef1b0c449d
x-dr <xd9722@gmail.com>
2023-12-21 00:31:31 +0800

    fix

---
+++ rm_l.py
@@ -0,0 +1,11 @@
+def remove_duplicates(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    unique_lines = list(set(lines))
+
+    with open(filename, 'w') as file:
+        file.writelines(unique_lines)
+
+# 调用函数并传入文件名
+remove_duplicates('ipb.txt')
